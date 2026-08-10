805fc4256910d62e69d7c0dda6d29b5427e15572
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-08-10 13:51:10 +0200

    Add cors middleware

--- patientor/backend/app.ts
+++ patientor/backend/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 import pingRouter from "./controllers/ping.ts";
 
 const app = express();
+
+app.use(cors());
 
 app.use('/api/ping', pingRouter);
 
--- patientor/backend/index.ts
+++ patientor/backend/index.ts
@@ -1,6 +1,6 @@
 import app from "./app.ts";
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`server listening at :${PORT}`);
