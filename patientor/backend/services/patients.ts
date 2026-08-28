1a1da1a6849092a894bf3229d9fb8633d7ab3dee
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-08-28 14:59:06 +0200

    Add new types

--- patientor/backend/services/patients.ts
+++ patientor/backend/services/patients.ts
@@ -1,10 +1,12 @@
 import { v1 as uuid } from "uuid";
 import data from "../data/patients.ts" with { type: "json" };
-import type { NewPatientEntry, Patient, PatientWithoutSSN } from "../types.ts";
+import type { NewPatientEntry, NonSensitivePatient, Patient, PatientWithoutEntries } from "../types.ts";
+import { PatientsListSchema } from "../types.ts";
 
-const patients: Patient[] = data;
+const patientsWithoutEntries: PatientWithoutEntries[] = data;
+const patients = PatientsListSchema.parse(patientsWithoutEntries.map((p) => ({ ...p, entries: [] })));
 
-const getEntries = (): PatientWithoutSSN[] => {
+const getEntries = (): NonSensitivePatient[] => {
   return patients.map((patient) => (
     {
       id: patient.id,
@@ -18,8 +20,8 @@ const getEntries = (): PatientWithoutSSN[] => {
 
 const addEntry = (patient: NewPatientEntry): Patient => {
   const id = uuid();
-  patients.push({ ...patient, id });
-  return { ...patient, id };
+  patients.push({ ...patient, id, entries: [] });
+  return { ...patient, id, entries: [] };
 };
 
 export default { getEntries, addEntry };
--- patientor/backend/types.ts
+++ patientor/backend/types.ts
@@ -6,17 +6,32 @@ export interface Diagnose {
   latin?: string;
 };
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface Entry {
+}
+
 export interface Patient {
+  id: string;
+  name: string;
+  ssn: string;
+  occupation: string;
+  gender: Gender;
+  dateOfBirth: string;
+  entries: Entry[];
+}
+
+export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
+
+export interface PatientWithoutEntries {
   id: string;
   name: string;
   dateOfBirth: string;
   gender: string;
   occupation: string;
-  ssn?: string;
+  ssn: string;
 };
 
-export type PatientWithoutSSN = Omit<Patient, "ssn">;
-export type NewPatientEntry = Omit<Patient, "id">;
+export type NewPatientEntry = Omit<Patient, "id" | "entries">;
 
 export const Gender = {
   Male: "male",
@@ -29,7 +44,17 @@ export type Gender = typeof Gender[keyof typeof Gender];
 export const NewPatientSchema = z.object({
   name: z.string(),
   dateOfBirth: z.iso.date(),
-  ssn: z.string().optional(),
+  ssn: z.string(),
   gender: z.enum(Gender),
   occupation: z.string(),
 });
+
+export const PatientsListSchema = z.array(z.object({
+  id: z.string(),
+  name: z.string(),
+  ssn: z.string(),
+  occupation: z.string(),
+  gender: z.enum(Gender),
+  dateOfBirth: z.string(),
+  entries: z.array(z.object({})),
+}))
